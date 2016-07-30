84707b629578e8b46ba840a90f7e15196728860d
Jiongkui <yinyuepingguo@qq.com>
2016-07-30 15:13:15 +0800

    add base-types.h for base.h

---
+++ include/base/base-types.h
@@ -0,0 +1,11 @@
+#ifndef _SINS_BASE_BASE_H
+#   error "use base/base-types.h outside base/base.h"
+#endif
+
+typedef unsigned char   schar;
+typedef int             sint;
+typedef unsigned int    suint;
+typedef long            slong;
+typedef unsigned long   sulong;
+
+typedef sint            sbool;
